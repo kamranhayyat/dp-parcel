8627dffe49b527667e3d1d86f8d9677c17cee846
kamran <kamranhayyat93@gmail.com>
2024-07-20 22:16:09 +0500

    parcel cities change

--- public/backend/js/merchant_panel/parcel/create.js
+++ public/backend/js/merchant_panel/parcel/create.js
@@ -150,7 +150,7 @@ function deliveryCharge() {
     var merchant_id            = $("select#merchant_id option").filter(":selected").val();
     var weight             = $("#weight").val();
     var delivery_type_id   = $("select#delivery_type_id option").filter(":selected").val();
-    var destination_district_id   = $("select#districts option").filter(":selected").val();
+    var destination_district_id   = $("select#district_id option").filter(":selected").val();
     var pickup_district_id   = $("#pickup_district_id").val();
     var delivery_distance   = $('#delivery_distance').val();
 
