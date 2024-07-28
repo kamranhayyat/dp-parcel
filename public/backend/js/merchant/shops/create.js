3ae57790d180a728d4ffae7b9745cb7a34c06aef
kamran <kamranhayyat93@gmail.com>
2024-07-28 15:38:42 +0500

    district dropdown changes

--- public/backend/js/merchant/shops/create.js
+++ public/backend/js/merchant/shops/create.js
@@ -1,19 +1,30 @@
 "use strict";
 
-$(document).on('change', '#district_id', function () {
-    $.ajax({
-        type : 'POST',
-        url : territoryCityUrl,
-        data : {'district_id': $(this).val()},
-        dataType : "html",
-        success : function (data) {
-            var $citySelect = $('#city_id');
-            $citySelect.find('option').remove();
-            var cities = JSON.parse(data)
-            for (const city of cities) {
-                $citySelect.append(`<option value=${city.id}>${city.name}</option>`);
-                console.log(city.name);
-            }
+$(document).ready(function () {
+    function loadCities() {
+        var districtId = $('#district_id').val();
+        if (districtId) {
+            $.ajax({
+                type: 'POST',
+                url: territoryCityUrl,
+                data: {'district_id': districtId},
+                dataType: 'html',
+                success: function (data) {
+                    var $citySelect = $('#city_id');
+                    $citySelect.empty(); // Clear existing options
+                    var cities = JSON.parse(data);
+                    cities.forEach(function(city) {
+                        $citySelect.append(`<option value="${city.id}">${city.name}</option>`);
+                    });
+                }
+            });
         }
-    });
+    }
+
+    // Trigger loadCities on page load
+    loadCities();
+
+    // Trigger loadCities when district selection changes
+    $(document).on('change', '#district_id', loadCities);
 });
+
